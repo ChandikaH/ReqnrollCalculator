af456b4bc9a5893bb26bc6aef56dbd21dd8f6054
Chandika Herath <chandikasbh@gmail.com>
2025-04-15 22:28:54 +1200

    updated project name

--- Features/Calculator.feature.cs
+++ Features/Calculator.feature.cs
@@ -10,7 +10,7 @@
 // ------------------------------------------------------------------------------
 #region Designer generated code
 #pragma warning disable
-namespace ReqnrollProject.Features
+namespace ReqnrollCalculator.Features
 {
     using Reqnroll;
     using System;
